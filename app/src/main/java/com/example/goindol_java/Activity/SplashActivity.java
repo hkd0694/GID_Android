29a56f09e83a6a82adf59f1b12cd41ab55bb68ff
hkd0694 <gksrudehd123@naver.com>
2019-12-16 12:47:47 +0900

    Splash Activity + initial commit

---
+++ app/src/main/java/com/example/goindol_java/Activity/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.goindol_java.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.example.goindol_java.MainActivity;
+import com.example.goindol_java.R;
+
+public class SplashActivity extends Activity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.imageView);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(imageView);
+        Glide.with(this).load(R.raw.splash).into(gifImage);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
